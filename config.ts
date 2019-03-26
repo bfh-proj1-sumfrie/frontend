161ba0db6ec97a2eacb25270f4ea9ca576219db8
Jan Friedli <jan.friedli@immerda.ch>
2019-03-26 22:17:05 +0100

    enable gziö

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 let api = process.env.API_URL;
-
+console.log(api)
 if (!api) {
   api = "https://bfh-proj1-backend.herokuapp.com"
 }
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-let express = require("express");
-let path = require("path");
-let serveStatic = require("serve-static");
+const express = require("express");
+const serveStatic = require("serve-static");
+const compression = require('compression')
+
 app = express();
+app.use(compression());
 app.use(serveStatic(__dirname + "/dist"));
 let port = process.env.PORT || 5000;
 app.listen(port);
