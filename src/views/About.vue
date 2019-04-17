f3723f774c805e14193752ebbebab67efb0938e6
Jan Friedli <jan.friedli@immerda.ch>
2019-04-17 21:54:01 +0200

    added new about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>BLOSQL</h1>
+    <p>
+      This project was created at the BFH in the project1 module.
+      The goal of it is to basically recreate the website
+      <a href="http://blockchainsql.io/" target="_blank">blockchainsql</a>.
+    </p>
+    <h2>
+      Authors
+    </h2>
+    <p>
+      Elias Summermatter, Jan Friedli
+    </p>
+    <h2>
+      Project Supervisor
+    </h2>
+    <p>
+      Kai Brünnler
+    </p>
+    <h2>Code</h2>
+    <p>
+      This project is open source and consist of two separate repositories.
+      A frontend based on Vue.js (Vuetify) which can be found here:
+      <a href="https://github.com/bfh-proj1-sumfrie/frontend"
+         target="_blank">https://github.com/bfh-proj1-sumfrie/frontend</a>
+      <br>
+      And a backend written in Python (Flask-restful):
+      <a href="https://github.com/bfh-proj1-sumfrie/backend"
+         target="_blank">https://github.com/bfh-proj1-sumfrie/backend</a>
+    </p>
   </div>
 </template>
+<style scoped>
+  a {
+    color: #000;
+  }
+</style>
