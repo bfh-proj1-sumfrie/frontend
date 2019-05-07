739acc56115a224208b58514758b25dabbbdf3cd
Elias Summermatter <elias.summermatter@seccom.ch>
2019-05-07 09:42:56 +0200

    Fixed linting

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const compression = require("compression");
-const history = require('connect-history-api-fallback');
+const history = require("connect-history-api-fallback");
 
 app = express();
-app.use(history())
+app.use(history());
 app.use(compression());
 app.use(serveStatic(__dirname + "/dist"));
 let port = process.env.PORT || 5000;
